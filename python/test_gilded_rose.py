e8116f08f51e5e8d2c36b7a58a6094010039b8c6
Diego Rivas <drivash@unsa.edu.pe>
2022-12-10 02:33:29 -0500

    Casos de prueba agregados

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,10 +6,21 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item("foo", 0, 0)]#Name,sell_in,quality
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
+    def test_equal_foo(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("foo", items[0].name)
+    def test_equal_quality(self):
+        items = [Item("No Aged Brie", -1, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        #print(items[0].quality)
+        self.assertEquals(8, items[0].quality)
 
         
 if __name__ == '__main__':
